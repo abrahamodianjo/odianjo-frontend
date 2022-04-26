70651a613ca5deac52c1c175f7a667392541805e
Abraham Odianjo <odianjoabraham@outlook.com>
2022-04-26 14:09:22 +0100

    i decided to change the function to a class to see if the command will work, if it does then this is the last commit for create user

--- src/components/Userlist.jsx
+++ src/components/Userlist.jsx
@@ -1,7 +1,6 @@
 import React,{useEffect,useState} from 'react'
 import axios from 'axios'
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function Userlist() {
 const [userId,setUserId] = useState('')
--- src/pages/Createuser.jsx
+++ src/pages/Createuser.jsx
@@ -1,87 +1,204 @@
-import React, { useState } from 'react'
+import React, { useState, Component } from 'react'
 import axios from 'axios';
 
 import Navigationbar from '../components/Navigationbar'
 import {toast} from 'react-toastify'
-import { useNavigate } from 'react-router-dom';
-function Createuser() {
+//import { useNavigate } from 'react-router-dom';
+// function Createuser() {
 
-const [loading, setloading] = useState(false);
-const [title, setTitle] = useState("");
-const [name, setName] = useState("");
-const [surname, setSurname] = useState("");
-const [email, setEmail] = useState("");
-const [city, setCity] = useState("");
+// const [loading, setloading] = useState(false);
+// const [title, setTitle] = useState("");
+// const [name, setName] = useState("");
+// const [surname, setSurname] = useState("");
+// const [email, setEmail] = useState("");
+// const [city, setCity] = useState("");
 
 
 //navigate
 
 
-const navigate = useNavigate()
+// const navigate = useNavigate()
 
-const handleSubmit = async(e) => {
-    e.preventDefault();
-    try{
-        setloading(true);
+// const handleSubmit = async(e) => {
+//     e.preventDefault();
+//     try{
+//         setloading(true);
 
-        //see that form is sending data 
-        // console.table({title,name,surname,email,city})
+//         //see that form is sending data 
+//         // console.table({title,name,surname,email,city})
 
-        //handle data and send to backend
-        const {data} = await axios.post('/odianjo/create/',{
-          title,name,email,city,surname  });
-        
-        setName(data.name);
-        setSurname(data.surname);
-        setEmail(data.email);
-        setCity(data.city);
-        setTitle(data.title);      
-        setloading(false);
-        toast.dark("User created successfully!")
-        navigate('/')
-    }catch(err){
-        console.log(err);
-        setloading(false)
-       toast.error(err.message)
-    }
-} 
-
-
+//         //handle data and send to backend
+//         const {data} = await axios.post('/odianjo/create/',{
+//           title,name,email,city,surname  });
+//           console.log("FETCHED >", data)
+//         setName("");
+//         setSurname("");
+//         setEmail("");
+//         setCity("");
+//         setTitle("");      
+//         setloading(false);
+//         toast.dark("User created successfully!")
+//         navigate('/')
+//     }catch(err){
+//         console.log(err);
+//         setloading(false)
+//        toast.error(err.message)
+//     }
+// } 
 
 
 
 
-  return (
-    <div>
-        <Navigationbar/>
 
 
-    {/* USER CREATION FORM */}
+//   return (
+//     <div>
+//         <Navigationbar/>
 
-    <div class="animate-pulse min-h-screen bg-slate-200 py-6 flex flex-col justify-center relative overflow-hidden sm:py-12">
-        <span class="border text-4xl text-yellow-800 px-6 pt-10 pb-8 bg-white w-1/2 max-w-md mx-auto rounded-t-md sm:px-10">Create User</span>
-  <div class="border relative px-4 pt-7 pb-8 bg-white shadow-xl w-1/2 max-w-md mx-auto sm:px-10 rounded-b-md">
-        <form onSubmit={handleSubmit}>
-            <label for="name" class="block">Name</label>
-            <input value={name} onChange={e=>setName(e.target.value)} type="text" class="border w-full h-10 px-3 mb-5 rounded-md" placeholder="Your name"/>
-            <label for="" class="block">Title</label>
-            <input type="text" value={title} onChange={e=>setTitle(e.target.value)}  class="border w-full h-10 px-3 mb-5 rounded-md" placeholder="Your Title"/>
-            <label for="" class="block">Surname</label>
-            <input type="text"  value={surname} onChange={e=>setSurname(e.target.value)} class="border w-full h-10 px-3 mb-5 rounded-md" placeholder="Your surname"/>
-            <label for="" class="block">City</label>
-            <input type="text"  value={city} onChange={e=>setCity(e.target.value)} class="border w-full h-10 px-3 mb-5 rounded-md" placeholder="Your city"/>
-            <label for="" class="block">Email</label>
-            <input type="Email"  value={email} onChange={e=>setEmail(e.target.value)} class="border w-full h-10 px-3 mb-5 rounded-md" placeholder="Your email"/>
+
+//     {/* USER CREATION FORM */}
+
+//     <div class="animate-pulse min-h-screen bg-slate-200 py-6 flex flex-col justify-center relative overflow-hidden sm:py-12">
+//         <span class="border text-4xl text-yellow-800 px-6 pt-10 pb-8 bg-white w-1/2 max-w-md mx-auto rounded-t-md sm:px-10">Create User</span>
+//   <div class="border relative px-4 pt-7 pb-8 bg-white shadow-xl w-1/2 max-w-md mx-auto sm:px-10 rounded-b-md">
+//         <form onSubmit={handleSubmit}>
+//             <label for="name" class="block">Name</label>
+//             <input value={name} npm install axios
+//             <input type="text" value={title} onChange={e=>setTitle(e.target.value)}  class="border w-full h-10 px-3 mb-5 rounded-md" placeholder="Your Title"/>
+//             <label for="" class="block">Surname</label>
+//             <input type="text"  value={surname} onChange={e=>setSurname(e.target.value)} class="border w-full h-10 px-3 mb-5 rounded-md" placeholder="Your surname"/>
+//             <label for="" class="block">City</label>
+//             <input type="text"  value={city} onChange={e=>setCity(e.target.value)} class="border w-full h-10 px-3 mb-5 rounded-md" placeholder="Your city"/>
+//             <label for="" class="block">Email</label>
+//             <input type="Email"  value={email} onChange={e=>setEmail(e.target.value)} class="border w-full h-10 px-3 mb-5 rounded-md" placeholder="Your email"/>
            
-					<div class="flex items-start">
+// 					<div class="flex items-start">
 					
-						</div>
-          <button type="submit" class="mt-5 bg-green-500 hover:bg-blue-700 shadow-xl text-white uppercase text-sm font-semibold px-14 py-3 rounded">Create User</button>
-        </form>
-  </div>
-</div>
-    </div>
-  )
-}
+// 						</div>
+//           <button type="submit" class="mt-5 bg-green-500 hover:bg-blue-700 shadow-xl text-white uppercase text-sm font-semibold px-14 py-3 rounded">Create User</button>
+//         </form>
+//   </div>
+// </div>
+//     </div>
+//   )
+// }
 
-export default Createuser
+// export default Createuser
+
+
+
+
+export default class Create extends Component {
+  
+    constructor(props) {
+        super(props);
+        this.onChangeTitle = this.onChangeTitle.bind(this);
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeSurname = this.onChangeSurname.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangeCity = this.onChangeCity.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+      
+        this.state = {
+            title: '',
+            name: '',
+            surname: '',
+            email: '',
+            city: ''
+        }
+      
+    }
+
+
+
+
+
+    onChangeTitle(e) {
+        this.setState({
+            title: e.target.value
+        });
+    }
+    onChangeName(e) {
+        this.setState({
+            name: e.target.value
+        });
+    }
+    onChangeSurname(e) {
+      this.setState({
+          surname: e.target.value
+      });
+      }
+      onChangeEmail(e) {
+        this.setState({
+            email: e.target.value
+        });
+    }
+    onChangeCity(e) {
+      this.setState({
+          city: e.target.value
+      });
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        const serverport = {
+            title: this.state.title,
+            name: this.state.name,
+            surname: this.state.surname,
+            email: this.state.email,
+            city: this.state.city
+        }
+        axios.post('/odianjo/create/', serverport)
+        .then(res => console.log(res.data));
+   
+        toast.dark("User created successfully!")
+        //         navigate('/')
+        this.setState({
+            title: '',
+            name: '',
+            surname: '',
+            email: '',
+            city: ''
+        });
+    }
+
+    render() {
+        return (
+          <div>
+          <Navigationbar/>
+               <div class="animate-pulse min-h-screen bg-slate-200 py-6 flex flex-col justify-center relative overflow-hidden sm:py-12">
+         <span class="border text-4xl text-yellow-800 px-6 pt-10 pb-8 bg-white w-1/2 max-w-md mx-auto rounded-t-md sm:px-10">Create User</span>
+        <div class="border relative px-4 pt-7 pb-8 bg-white shadow-xl w-1/2 max-w-md mx-auto sm:px-10 rounded-b-md"> 
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Title:  </label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Surname: </label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Name: </label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Email: </label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label>City: </label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Add Data" className="btn btn-danger"/>
+                    </div>
+                </form>
+            </div>
+            </div>
+          
+            </div>
+           
+           )
+    }
+}
